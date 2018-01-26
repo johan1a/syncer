f3f9e1817c7ba32d88aade2aa8b77c2c58ab0df4
Johan Andersson <joha@netcompany.com>
2018-01-26 22:32:06 +0100

    change jenkinsfile

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,5 +5,5 @@ ssh -i /var/jenkins_home/.ssh_host/id_rsa johan@urdatorn 'mkdir -p ~/syncer/data
 echo whyyyy2
 scp -i /var/jenkins_home/.ssh_host/id_rsa docker-compose.yml johan@urdatorn:syncer/
 echo whyyyy3
-ssh -i /var/jenkins_home/.ssh_host/id_rsa johan@urdatorn 'export SYNCER_HOSTS=archlinux cd ~/syncer/ && SYNCER_HOSTS=archlinux  docker-compose up'
+ssh -i /var/jenkins_home/.ssh_host/id_rsa johan@urdatorn 'export SYNCER_HOSTS=archlinux cd && cd syncer/ && SYNCER_HOSTS=archlinux docker-compose up'
 
