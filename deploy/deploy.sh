b3c54f243491af3cef01efbc6f083023357456e4
Johan Andersson <joha@netcompany.com>
2018-01-26 21:23:49 +0100

    change deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ssh johan@urdatorn 'mkdir -p ~/syncer/data/'
-scp docker-compose.yml johan@urdatorn:syncer/
-ssh johan@urdatorn 'cd ~/syncer/ && docker-compose up'
+ssh -i /var/lib/jenkins_home/.ssh_host/id_rsa johan@urdatorn 'mkdir -p ~/syncer/data/'
+scp -i /var/lib/jenkins_home/.ssh_host/id_rsa docker-compose.yml johan@urdatorn:syncer/
+ssh -i /var/lib/jenkins_home/.ssh_host/id_rsa johan@urdatorn 'cd ~/syncer/ && docker-compose up'
 
