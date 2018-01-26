6acdd50309866f6faae6e966ecc9f9910d5f3d03
Johan Andersson <joha@netcompany.com>
2018-01-26 22:39:31 +0100

    push latest

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,5 +5,6 @@ ssh -i /var/jenkins_home/.ssh_host/id_rsa johan@urdatorn 'mkdir -p ~/syncer/data
 echo whyyyy2
 scp -i /var/jenkins_home/.ssh_host/id_rsa docker-compose.yml johan@urdatorn:syncer/
 echo whyyyy3
+ssh -i /var/jenkins_home/.ssh_host/id_rsa johan@urdatorn 'cd syncer/ && SYNCER_HOSTS=archlinux docker-compose pull'
 ssh -i /var/jenkins_home/.ssh_host/id_rsa johan@urdatorn 'export SYNCER_HOSTS=archlinux cd && cd syncer/ && SYNCER_HOSTS=archlinux docker-compose up'
 
